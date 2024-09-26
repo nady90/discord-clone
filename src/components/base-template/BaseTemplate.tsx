1cf3e5229052cacc4dedc500f8eca25ea0e2911f
Abanoub <naguib.abanoub@gmail.com>
2024-09-26 13:33:09 +0300

    feat: added story book

---
+++ src/components/base-template/BaseTemplate.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export interface ButtonProps {
+  /** Is this the principal call to action on the page? */
+  primary?: boolean;
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: "small" | "medium" | "large";
+  /** Button contents */
+  label: string;
+  /** Optional click handler */
+  onClick?: () => void;
+}
+
+/** Primary UI component for user interaction */
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+
+  console.log(size, mode, backgroundColor, label);
+  return (
+    <button type="button" className="bg-red-500 text-blue-500" {...props}>
+      Hello
+    </button>
+  );
+};
